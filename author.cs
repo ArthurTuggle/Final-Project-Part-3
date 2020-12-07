d7a76fc6f9363c687e0436aa123724e20b9a529f
ArthurTuggle <arthurtuggle57@yahoo.com>
2020-12-07 18:42:36 +0000

    errors

--- author.cs
+++ author.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 using System.Globalization;
 
-public class Author:Person
+public class Author
 
 {
 
--- patron.cs
+++ patron.cs
@@ -6,7 +6,7 @@ namespace RentalApp
 
 {
 
-internal class Patron : Person
+internal class Patron
 
 {
 
